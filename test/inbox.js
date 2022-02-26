dfd8ca96c2f9b4eb84cbe6b33fea9209b700e8c2
genialtechie <thehaleembello@gmail.com>
2022-02-26 01:31:21 -0500

    My First DApp

--- test/inbox.js
+++ test/inbox.js
@@ -1,6 +1,6 @@
 const Inbox = artifacts.require("Inbox");
 
-contract("Inbox", async function(accounts){
+contract("Inbox", function(accounts){
     it("test set message", async function() {
         const instance = await Inbox.deployed();
         const setMess = await instance.setMessage('How are you doing', {from: accounts[1]});
